7349ce7812716f52e2d440344f5115e7d16c55ea
Luis Felipe <slashcat@gmail.com>
2012-06-26 00:38:47 -0500

    Adaptación para uso de código de Barras

--- application/modules/registros/views/nuevo.php
+++ application/modules/registros/views/nuevo.php
@@ -109,3 +109,15 @@
 	</div>  
 </form>
 
+<script type="text/javascript">
+$(function() {
+	$('input').keydown( function(e) {
+        var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+        if(key == 13) {
+            e.preventDefault();
+            var inputs = $(this).closest('form').find(':input:visible');
+            inputs.eq( inputs.index(this)+ 1 ).focus();
+        }
+    });
+});
+</script>
